c14466e6b70c2538f506b3b35f6e377802e602e0
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-04 21:31:51 +0600

    first commit

---
+++ function.c
@@ -0,0 +1,47 @@
+// #include <stdio.h>
+// declaration
+// void helloPrint();
+
+
+// int main(){
+//       helloPrint() ;  // function call 
+//     return 0;
+// }
+
+// Function Dafination
+// void helloPrint(){
+//     printf("Hello - World\n");
+// }
+
+
+
+
+
+#include <stdio.h>
+void  printHello();
+int main(){
+  printHello();
+  printHello();
+  printHello();
+
+    return 0;
+}
+void printHello(){
+    printf("Hello ! \n");
+    printf("Learn'C' Programming Language \n") ;
+}
+
+
+
+
+
+
+
+
+
+
+// #include <stdio.h>
+// int main(){
+
+//     return 0;
+// }
---
+++ solve_problem.c
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+// write 2 functions one to print "Hello"  and Second to print "Goodbey"
+
+#include <stdio.h>
+void hello_f();
+void bey_f();
+int main(){
+
+    hello_f();
+    bey_f();
+    return 0;
+}
+
+void hello_f(){
+printf("Hello \n");
+}
+void bey_f(){
+    printf("goodbey :) \n");
+}
+
+
+
+
+
+
+// Write a fuction that prints Salam if user is Bagladashi 
+// &  Hey if the user is French
+
+
+void salam_f();
+void hey_f();
+
+#include <stdio.h>
+int main(){
+      printf("Enter f  for french &  b for bagladesh :");
+      int ch;
+      scanf("%ch",&ch);
+      if(ch == 'b'){
+        salam_f();
+      }else{
+        hey_f();
+      }
+    return 0;
+}
+
+void salam_f(){
+    printf("Assalamualaiqum\n");
+}
+
+void hey_f(){
+    printf("Hey ! \n");
+}
+
+
+
+// #include <stdio.h>
+// int main(){
+
+//     return 0;
+// }
