bf259e6983bc370dd86b3c154969a061b0ec665d
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-14 08:40:18 +0600

    solve 8th function in C

--- solve_problem.c
+++ solve_problem.c
@@ -5,86 +5,86 @@
 
 // 👉👉  🔹Qu -1 🔹 write 2 functions one to print "Hello"  and Second to print "Goodbey"
 
-#include <stdio.h>
-void hello_f();
-void bey_f();
-int main(){
+// #include <stdio.h>
+// void hello_f();
+// void bey_f();
+// int main(){
 
-    hello_f();
-    bey_f();
-    return 0;
-}
+//     hello_f();
+//     bey_f();
+//     return 0;
+// }
 
-void hello_f(){
-printf("Hello \n");
-}
-void bey_f(){
-    printf("goodbey :) \n");
-}
+// void hello_f(){
+// printf("Hello \n");
+// }
+// void bey_f(){
+//     printf("goodbey :) \n");
+// }
 
 // 👉👉  🔹Qu -2 🔹  Write a fuction that prints Salam if user is Bagladashi
 //  &  Hey if the user is French
 
-#include <stdio.h>
-void salam_f();
-void hey_f();
-int main(){
-      printf("Enter f  for french &  b for bagladesh :");
-      int ch;
-      scanf("%ch",&ch);
-      if(ch == 'b'){
-        salam_f();
-      }else{
-        hey_f();
-      }
-    return 0;
-}
+// #include <stdio.h>
+// void salam_f();
+// void hey_f();
+// int main(){
+//       printf("Enter f  for french &  b for bagladesh :");
+//       int ch;
+//       scanf("%ch",&ch);
+//       if(ch == 'b'){
+//         salam_f();
+//       }else{
+//         hey_f();
+//       }
+//     return 0;
+// }
 
-void salam_f(){
-    printf("Assalamualaiqum\n");
-}
+// void salam_f(){
+//     printf("Assalamualaiqum\n");
+// }
 
-void hey_f(){
-    printf("Hey ! \n");
-}
+// void hey_f(){
+//     printf("Hey ! \n");
+// }
 
 // 👉👉  🔹Qu -3 🔹 use library fucntions to calculate the square of a number given by the user
 
-#include <stdio.h>
-#include <math.h>
+// #include <stdio.h>
+// #include <math.h>
 
-int main(){
-int n = 4 ;
-printf("%f\n", pow(n,2));
-    return 0;
-}
+// int main(){
+// int n = 4 ;
+// printf("%f\n", pow(n,2));
+//     return 0;
+// }
 //  ======================================= 
 
-#include <stdio.h>
-#include <math.h>
+// #include <stdio.h>
+// #include <math.h>
 
- float square_f( float side);
- float  circle_f(float rad);
- float  rectangle_f(float a , float b);
-int main(){
+//  float square_f( float side);
+//  float  circle_f(float rad);
+//  float  rectangle_f(float a , float b);
+// int main(){
 
-    float a = 4;
-    float b = 4;
-    printf("Total Area is : %f\n" , rectangle_f(a,b));
-    return 0;
-}
+//     float a = 4;
+//     float b = 4;
+//     printf("Total Area is : %f\n" , rectangle_f(a,b));
+//     return 0;
+// }
 
-float square_f(float side){
-         return side * side ;
-}
+// float square_f(float side){
+//          return side * side ;
+// }
 
-float cirlce_f(float rad){
-    return  3.1416 * rad * rad ;
-}
+// float cirlce_f(float rad){
+//     return  3.1416 * rad * rad ;
+// }
 
-float rectangle_f(float a , float b){
-    return  a*b ;
-}
+// float rectangle_f(float a , float b){
+//     return  a*b ;
+// }
 //  =====================================  //  ======================
 
 // ============================================================
@@ -97,134 +97,134 @@ float rectangle_f(float a , float b){
 // 👉👉 🔹 Q-4: একটি ফাংশন লিখো যা একটি পূর্ণসংখ্যা ইনপুট নিয়ে তার স্কয়ার রিটার্ন করবে।
 // ============================================================
 
-#include <stdio.h>
- int square (int n);
-int main(){
-    int num = 40;
-     int result =  square(num);
-     printf("Square Reult is : %d\n ", result);
+// #include <stdio.h>
+//  int square (int n);
+// int main(){
+//     int num = 40;
+//      int result =  square(num);
+//      printf("Square Reult is : %d\n ", result);
 
-    return 0;
-}
+//     return 0;
+// }
 
-int square(int n ) {
-    return n*n ;
-}
+// int square(int n ) {
+//     return n*n ;
+// }
 
 // ============================================================
 // 👉👉 🔹 Q-: একটি ফাংশন লিখো যা একটি পূর্ণসংখ্যা ইনপুট নিয়ে তার স্কয়ার রিটার্ন করবে।
 // ============================================================
-#include <stdio.h>
-int input_square(int n);
-int main(){
-        int n ;
-        printf("Enter Number : ");
-        scanf("%d" , &n);
-    int  result  = input_square(n);
-    printf("Square number is : %d\n", result) ;
-    return 0;
-}
+// #include <stdio.h>
+// int input_square(int n);
+// int main(){
+//         int n ;
+//         printf("Enter Number : ");
+//         scanf("%d" , &n);
+//     int  result  = input_square(n);
+//     printf("Square number is : %d\n", result) ;
+//     return 0;
+// }
 
-int input_square(int n){
-   return  n*n ;
-}
+// int input_square(int n){
+//    return  n*n ;
+// }
 
 // ============================================================
 // 👉👉 🔹 Q- 5: একটি ফাংশন লিখো যা একটি পূর্ণসংখ্যা ইনপুট নিয়ে বলে দেবে সংখ্যাটি জোড় না বিজোড়।
 // ============================================================
 
-#include <stdio.h>
-void checkEvenOdd(int  n );
-int main(){
+// #include <stdio.h>
+// void checkEvenOdd(int  n );
+// int main(){
 
-    int n;
-    printf("Enter Number ");
-    scanf("%d", &n);
+//     int n;
+//     printf("Enter Number ");
+//     scanf("%d", &n);
 
-    checkEvenOdd(n);
-    return 0;
-}
-void checkEvenOdd(int n){
-      if (n % 2 == 0  )
-      {
-       printf("%d is Even.\n " , n) ;
-      }else{
-        printf("%d is Odd.\n " , n) ;
-      }
+//     checkEvenOdd(n);
+//     return 0;
+// }
+// void checkEvenOdd(int n){
+//       if (n % 2 == 0  )
+//       {
+//        printf("%d is Even.\n " , n) ;
+//       }else{
+//         printf("%d is Odd.\n " , n) ;
+//       }
 
-}
+// }
 
 // ===========================================================
 //             📌  Review Solve
 // 👉👉 🔹 Q- 5: একটি ফাংশন লিখো যা একটি পূর্ণসংখ্যা ইনপুট নিয়ে বলে দেবে সংখ্যাটি জোড় না বিজোড়।
 // ============================================================
 
-#include <stdio.h>
-void  checkEvenOdd(int n);
-int main(){
-int n;
-printf("Enter Number  : ");
-scanf("%d" , &n);
-checkEvenOdd(n);
-    return 0;
-}
+// #include <stdio.h>
+// void  checkEvenOdd(int n);
+// int main(){
+// int n;
+// printf("Enter Number  : ");
+// scanf("%d" , &n);
+// checkEvenOdd(n);
+//     return 0;
+// }
 
-void checkEvenOdd(int n){
-   if (n % 2 == 0 )
-   {
-    printf("%d is Even.\n" , n);
-   }else{
-    printf("%d is Odd.\n" , n);
-   }
+// void checkEvenOdd(int n){
+//    if (n % 2 == 0 )
+//    {
+//     printf("%d is Even.\n" , n);
+//    }else{
+//     printf("%d is Odd.\n" , n);
+//    }
 
-}
+// }
 
 // ============================================================
 //         📌   Sum of Two Numbers
 // 👉👉 🔹 Q- 6:  দুটি সংখ্যার যোগফল বের করার জন্য একটি ফাংশন লিখো।
 // ============================================================
 
-#include <stdio.h>
-int sum(int y, int z);
+// #include <stdio.h>
+// int sum(int y, int z);
 
-int main(){
-int y,z;
-printf("Enter Tow Numbers : ");
-scanf(" %d %d", &y, &z);
+// int main(){
+// int y,z;
+// printf("Enter Tow Numbers : ");
+// scanf(" %d %d", &y, &z);
 
-int result = sum(y,z) ;
-printf("Result is : %d\n", result);
+// int result = sum(y,z) ;
+// printf("Result is : %d\n", result);
 
-    return 0;
+//     return 0;
 
-}
-int sum(int y , int z){
-    return y+z;
-}
+// }
+// int sum(int y , int z){
+//     return y+z;
+// }
 // ============================================================
 //         📌   Sum of Two Numbers
 // 👉👉 🔹 Q- 6.2:  দুটি সংখ্যার যোগফল বের করার জন্য একটি ফাংশন লিখো।
 //          👉 🔹 Version :2.0
 // ============================================================
-#include <stdio.h>
-int sum(int a , int b);
-int main(){
-int a ;
-int b ;
-printf("Enter first  Numbers :  ");
-scanf("%d", &a );
-printf("Enter second  Numbers :  ");
-scanf("%d",  &b);
+// #include <stdio.h>
+// int sum(int a , int b);
+// int main(){
+// int a ;
+// int b ;
+// printf("Enter first  Numbers :  ");
+// scanf("%d", &a );
+// printf("Enter second  Numbers :  ");
+// scanf("%d",  &b);
 
-int total= sum(a,b);
-printf("Sum is : %d\n", total);
+// int total= sum(a,b);
+// printf("Sum is : %d\n", total);
 
-    return 0;
-}
+//     return 0;
+// }
 
-int sum(int a , int b){
-    return a + b ;
-}
+// int sum(int a , int b){
+//     return a + b ;
+// }
 
 // ============================================================
 //                    📌 Practice Problems
@@ -239,26 +239,26 @@ int sum(int a , int b){
 // 👉👉 🔹 Q- 7:  একটি ফাংশন লিখো যা একটি সংখ্যা ইনপুট নিয়ে তার Factorial বের করবে।
 // ============================================================
 
-#include <stdio.h>
-int factorial(int n);
-int main(){
- int n ;
- printf("Enter Number ") ;
- scanf("%d" , &n);
- int fac = factorial(n);
- printf("factorial is %d\n" , fac);
+// #include <stdio.h>
+// int factorial(int n);
+// int main(){
+//  int n ;
+//  printf("Enter Number ") ;
+//  scanf("%d" , &n);
+//  int fac = factorial(n);
+//  printf("factorial is %d\n" , fac);
 
-    return 0;
-}
+//     return 0;
+// }
 
-int factorial(int n){
-    int result = 1 ;
-  for (int i = 1 ; i<=n ; i++){
-  result = i*i;
-  }
-  return result ;
+// int factorial(int n){
+//     int result = 1 ;
+//   for (int i = 1 ; i<=n ; i++){
+//   result = i*i;
+//   }
+//   return result ;
 
-}
+// }
 
 // // ============================================================
 //             📌   Factorial Finder
@@ -266,57 +266,114 @@ int factorial(int n){
 //    👉 🔹       Review Write code
 // ============================================================
 
-#include <stdio.h>
-int factorial(int a) ;
-int main(){
-    int a ;
-    printf("Enter Number :");
-    scanf("%d",&a);
-   int  fact  = factorial(a);
-   printf("Factorial is %d\n" , fact);
-    return 0;
-}
+// #include <stdio.h>
+// int factorial(int a) ;
+// int main(){
+//     int a ;
+//     printf("Enter Number :");
+//     scanf("%d",&a);
+//    int  fact  = factorial(a);
+//    printf("Factorial is %d\n" , fact);
+//     return 0;
+// }
 
-int factorial(int a ){
-    int  result = 1;
-    for(int i = 1; i<=a; i++){
-        result = i*i;
-    }
+// int factorial(int a ){
+//     int  result = 1;
+//     for(int i = 1; i<=a; i++){
+//         result = i*i;
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
 // ============================================================
 //             📌  Prime Number Checker
 // 👉👉 🔹 Q- 7:  একটি ফাংশন লিখো যা একটি সংখ্যা ইনপুট নিয়ে বলে দেবে সেটি Prime কিনা
 // ============================================================
 
+// #include <stdio.h>
+// int isPrime(int a);
+// int main()
+// {
+//     int a;
+//     printf("Enter Number ");
+//     scanf("%d", &a);
+//     int result = isPrime(a);
+//     printf("prime number is %d\n" , result);
+//     return 0;
+// }
+
+// int isPrime(int a)
+// {
+//     if (a <= 1)
+//     {
+//         printf("Is not a prime  number %d", a);
+//         return 0;
+//     }
+//     for (int i = 2; i * i; i++)
+//     {
+//         if (a % i == 0)
+//         {
+//             return 0;
+//         }
+//     }
+
+//     return 1;
+// }
+
+
+
+
+
+//    👉👉  🔹🔹Qu -  8️⃣  Write a fucntion convart  celsius to fahrenheit ..
+
+
 #include <stdio.h>
-int isPrime(int a);
-int main()
-{
-    int a;
-    printf("Enter Number ");
-    scanf("%d", &a);
-    int result = isPrime(a);
-    printf("prime number is %d\n" , result);
+//  Fuction Prototype
+float con_Tem(float cel );
+int main(){
+     
+    float feh = con_Tem(0); // function call
+    printf("Temp is %f\n" , feh);
     return 0;
 }
+//  Function Diclaration 
 
-int isPrime(int a)
-{
-    if (a <= 1)
-    {
-        printf("Is not a prime  number %d", a);
-        return 0;
-    }
-    for (int i = 2; i * i; i++)
-    {
-        if (a % i == 0)
-        {
-            return 0;
-        }
-    }
-
-    return 1;
+float con_Tem( float cel ){
+    float feh = cel *(5.0 / 9.0) + 32 ;
+    return feh ;
 }
+
+
+
+
+
+
+
+// #include <stdio.h>
+// float Con_Temp( float cel);
+// int main(){
+//  float feh =  Con_Temp(0);   // Function call 
+//  printf("celsius is %f\n" , feh);
+
+//     return 0;
+// }
+// //   Fuction dafiantion 
+// float  Con_Temp( float cel){
+//    float feh = cel * (9.0 / 5.0) + 32 ;
+//    return feh ; 
+// }
+
+
+// #include <stdio.h>
+// int main(){
+
+//     return 0;
+// }
+
+
+
+//   Number Emoji     👉👉  🔹Qu -🔹 
+//    9️⃣ 🔟    
+// 1️⃣1️⃣ 1️⃣2️⃣ 1️⃣3️⃣ 1️⃣4️⃣ 1️⃣5️⃣ 1️⃣6️⃣ 1️⃣7️⃣ 1️⃣8️⃣ 1️⃣9️⃣ 2️⃣0️⃣
+// 2️⃣1️⃣ 2️⃣2️⃣ 2️⃣3️⃣ 2️⃣4️⃣ 2️⃣5️⃣ 2️⃣6️⃣ 2️⃣7️⃣ 2️⃣8️⃣ 2️⃣9️⃣ 3️⃣0️⃣
